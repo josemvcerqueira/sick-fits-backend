be51e28734ac0db731b4cad797e297f6611fd696
Jose Cerqueira <jose@origensmedia.com>
2019-06-04 18:20:26 +0100

    Set up the mutation and schema to create a User

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: ".env" });
 
 const createServer = require("./createServer");
@@ -5,7 +6,8 @@ const db = require("./db");
 
 const server = createServer();
 
-// TODO Use express middleware to handle cookies (JWT)
+//  express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
 // TODO Use express middleware to populate current user
 
 server.start(
