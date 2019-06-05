1e8cde4930d30aa2e3506ab1c781c457e6ebab88
Jose Cerqueira <jose@origensmedia.com>
2019-06-05 09:00:59 +0100

    Set up the mutation to sign out

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -85,6 +85,10 @@ const Mutations = {
 		});
 		// 5. return the user
 		return user;
+	},
+	signout(parent, args, ctx, info) {
+		ctx.response.clearCookie("token");
+		return { message: "Goodbye!" };
 	}
 };
 
