bfeba1ab221e4c1b577125616600a1530bc243f0
Jose Cerqueira <jose@origensmedia.com>
2019-06-05 13:46:08 +0100

    Set up the mail functionality

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,6 +3,8 @@ const jwt = require("jsonwebtoken");
 const { randomBytes } = require("crypto");
 const { promisify } = require("util");
 
+const { transport, makeANiceEmail } = require("../mail");
+
 const Mutations = {
 	async createItem(parent, args, ctx, info) {
 		const item = await ctx.db.mutation.createItem(
@@ -107,6 +109,18 @@ const Mutations = {
 			data: { resetToken, resetTokenExpiry }
 		});
 		// 3. Email them that reset token
+		const mailRes = await transport.sendMail({
+			from: "jose@gmail.com",
+			to: user.email,
+			subject: "Your Password Reset",
+			html: makeANiceEmail(`Your Password Reset Token is here! \n\n 
+				<a href="${
+					process.env.FRONTEND_URL
+				}/reset?resetToken=${resetToken}">Click Here to Reset</a>
+				`)
+		});
+
+		// 4. Return the message
 		return { message: "Thank you!" };
 	},
 	async resetPassword(parent, args, ctx, info) {
