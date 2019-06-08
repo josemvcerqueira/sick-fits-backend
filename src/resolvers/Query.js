df0788c6aee87396803fb8b282d6384c50afe450
Jose Cerqueira <jose@origensmedia.com>
2019-06-08 23:41:50 +0100

    Set up the data model and query for order items

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -27,6 +27,29 @@ const Query = {
 		hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
 		// 3. if they do, query all the users
 		return await ctx.db.query.users({}, info);
+	},
+	async order(parent, args, ctx, info) {
+		// 1. Make sure they are logged in
+		if (!ctx.request.userId) {
+			throw new Error("You arent logged in!");
+		}
+		// 2. Query the current order
+		const order = await ctx.db.query.order(
+			{
+				where: { id: args.id }
+			},
+			info
+		);
+		// 3. Check if they have the permissions to see this order
+		const ownsOrder = order.user.id === ctx.request.userId;
+		const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+			"ADMIN"
+		);
+		if (!ownsOrder || !hasPermissionToSeeOrder) {
+			throw new Error("You can't see this fella");
+		}
+		// 4. Return the order
+		return order;
 	}
 };
 
