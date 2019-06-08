41a0336dd8ca2446b60bb2e64fc7120b6b0a4d85
Jose Cerqueira <jose@origensmedia.com>
2019-06-08 17:28:54 +0100

    Added the createOrder Mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -5,6 +5,7 @@ const { promisify } = require("util");
 
 const { transport, makeANiceEmail } = require("../mail");
 const { hasPermission } = require("../utils");
+const stripe = require("../stripe");
 
 const Mutations = {
 	async createItem(parent, args, ctx, info) {
@@ -267,6 +268,39 @@ const Mutations = {
 			},
 			info
 		);
+	},
+	async createOrder(parent, args, ctx, info) {
+		// 1. Query the current user and makes sure they are signed in
+		const { userId } = ctx.request;
+		if (!userId)
+			throw new Error("You must be signed in to complete this order");
+		const user = await ctx.db.query.user(
+			{ where: { id: userId } },
+			`{
+				id 
+				name 
+				email 
+				cart { 
+					id 
+					quantity 
+					item { title, price, id, description, image}
+				}}`
+		);
+		// 2. Recalculate the total for the price
+		const amount = user.cart.reduce(
+			(acc, cartItem) => acc + cartItem.item.price * cartItem.quantity,
+			0
+		);
+		// 3. Create the stripe charge (turn token into money)
+		const charge = await stripe.charges.create({
+			amount,
+			currency: "USD",
+			source: args.token
+		});
+		// 4. Convert the CartItems to OrderItems
+		// 5. Create the order
+		// 6. Clean up - clear the users cart, delete cartItems
+		// 7. Return the Order to the client
 	}
 };
 
